33aec5583d38ea171fcae117103016fb36d50dfc
meldsza <melroynd@live.com>
2017-07-22 15:03:09 +0530

    v0.1.2 - better tests

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -2,5 +2,13 @@ require 'spec_helper'
 describe 'discourse_deploy' do
   context 'with default values for all parameters' do
     it { should contain_class('discourse_deploy') }
+    it { is_expected.to compile.with_all_deps }
+    it { is_expected.to contain_package('docker').with(ensure: 'present') } 
+    it { is_expected.to contain_file('/var/discourse/containers/app.yml')
+        .with(
+          :ensure => 'file'
+        )
+      }
+      
   end
 end
