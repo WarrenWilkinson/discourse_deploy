3ff9eca183889ecc4f9adbb18b642bc54dc38fee
Melroy Neil Dsouza <meldsza@gmail.com>
2017-07-24 23:09:54 +0530

    Update init_spec.rb

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -3,7 +3,6 @@ describe 'discourse_deploy' do
   context 'with default values for all parameters' do
     it { should contain_class('discourse_deploy') }
     it { is_expected.to compile.with_all_deps }
-    it { is_expected.to contain_package('docker').with(ensure: 'present') } 
     it { is_expected.to contain_file('/var/discourse/containers/app.yml')
         .with(
           :ensure => 'file'
