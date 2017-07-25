a224c664392c1a4d74a103ee0cbce6b53fe45f45
meldsza <melroynd@live.com>
2017-07-25 22:49:04 +0530

    add build paths

--- manifests/init.pp
+++ manifests/init.pp
@@ -122,15 +122,17 @@ class discourse_deploy (
     content => epp("discourse_deploy/${type}.epp")
   }
   exec { 'build':
-    command     => '/var/discourse/launcher bootstrap app',
+    command     => 'sudo /var/discourse/launcher bootstrap app',
     cwd         => '/var/discourse/',
     subscribe   => File['/var/discourse/containers/app.yml'],
-    refreshonly => true
+    refreshonly => true,
+    path        => ['/usr/bin', '/usr/sbin']
   }
   exec { 'launch':
-    command     =>'/var/discourse/launcher start app',
+    command     =>'sudo /var/discourse/launcher start app',
     cwd         => '/var/discourse/',
     subscribe   => Exec['build'],
-    refreshonly => true
+    refreshonly => true,
+    path        => ['/usr/bin', '/usr/sbin']
   }
 }
